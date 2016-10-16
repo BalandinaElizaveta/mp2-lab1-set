e24704e43af5ec7b72b628d305573d79a5f7ab10
BalandinaElizaveta <balandina_liza@mail.ru>
2016-10-16 12:00:24 +0300

    теоретико-множественные операции, ввод/вывод

--- src/tset.cpp
+++ src/tset.cpp
@@ -55,52 +55,87 @@ void TSet::DelElem(const int Elem) // исключение элемента мн
 
 TSet& TSet::operator=(const TSet &s) // присваивание
 {
-
+	BitField = s.BitField;
+ 	MaxPower = s.MaxPower;
+ 	return *this;
 }
 
 int TSet::operator==(const TSet &s) const // сравнение
 {
-    return 0;
+    if (BitField != s.BitField) return 0;
+ 	return 1;
 }
 
 int TSet::operator!=(const TSet &s) const // сравнение
 {
-
+	if (BitField == s.BitField) return 0;
+ 	 return 1;
 }
 
 TSet TSet::operator+(const TSet &s) // объединение
 {
-
+	TSet pr(BitField | s.BitField);
+ 	return pr;
 }
 
 TSet TSet::operator+(const int Elem) // объединение с элементом
 {
-
+	if (Elem>MaxPower)
+ 	{
+ 		throw(Elem);
+ 		
+ 	}
+ 	TBitField res(MaxPower);
+ 	res.SetBit(Elem);
+ 	TSet pr(BitField | res);
+ 	return pr;
 }
 
 TSet TSet::operator-(const int Elem) // разность с элементом
 {
-
+	if (Elem>MaxPower)
+ 	{
+ 		throw(Elem);
+ 		
+ 	}
+ 	TBitField res(MaxPower);
+ 	res.SetBit(Elem);
+ 	TSet pr(BitField & res);
+ 	return pr;
 }
 
 TSet TSet::operator*(const TSet &s) // пересечение
 {
-
+	TSet pr(BitField & s.BitField);
+ 	return pr;
 }
 
 TSet TSet::operator~(void) // дополнение
 {
-
+	TSet result(~BitField);
+ 	return result;
 }
 
 // перегрузка ввода/вывода
 
 istream &operator>>(istream &istr, TSet &s) // ввод
 {
+	char k;
+ 	int i = 0;
+ 	for (i = 0; i < s.MaxPower; i++)
+ 	{
+ 		cin >> k;
+ 		if (k == '1') s.InsElem(i);
+ 		else s.DelElem(i);
+ 	}
+ 	return istr;
+ }
 
-}
 
 ostream& operator<<(ostream &ostr, const TSet &s) // вывод
 {
-
+	int i = 0;
+ 	for (i = 0; i < s.MaxPower; i++)
+ 		cout<< ' ' << s.IsMember(i);
+ 	return ostr;
 }
